3afe5b459c6995281b6a17f82c5673d4693b6237
Niclas L <niclas.lund@ntig.se>
2024-09-03 08:44:12 +0200

    code shell added

---
+++ main.py
@@ -0,0 +1,31 @@
+# Ordstatistik
+# Din uppgift är att läsa in text från filen som är angiven.
+# Därefter ska ditt program räkna ut följande:
+# - Antal ord
+# - Mest frekventa ord
+# - Genomsnittlig ordlängd
+# Gör en funktion för varje.
+
+# Bonus, gör en i taget, skapa en funktion för varje: 
+# - Längsta och kortaste ordet - om det finns flera, bestäm själv om du skriver ut ett eller flera
+# - Räkna antalet unika ord (alltså ord som bara förekommer en gång)
+
+
+def read_from_file(path: str):
+    """Reads a file with the given parameter path and returns the file as a list of strings, split on newline (\n).
+
+    Args:
+        path (str): the path of the readable file
+
+    Returns:
+        list(str): list of strings
+    """
+    with open(path, "r" ,encoding="utf-8") as f:
+        return f.readlines()
+
+def main():
+    
+    sentences = read_from_file("en_resa_genom_svenska_skogen.txt") # Här har du nu en lista av strängar från den inlästa filen.
+
+if __name__ == "__main__":
+    main()
